165927f55c9ed071ab316fd963b178e679be0dbe
pek1976 <enzo.suchetti@gmail.com>
2020-01-05 19:48:51 -0300

    Modificaciones en las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,31 +7,16 @@ import { APP_ROUTES } from './app.routes';
 //Modulos
 import { PagesModule } from './pages/pages.module';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-   // DashboardComponent,
-   // ProgressComponent,
-   // Graficas1Component,
-    //NopagefoundComponent,
-    //HeaderComponent,
-    //SidebarComponent,
-    //BreadcrumsComponent,
-    //PagesComponent,
     RegisterComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-    // {
-    //     path: '',
-    //     component: PagesComponent,
-    //     children: [
-    //         {path: 'dashboard', component: DashboardComponent},
-    //         {path: 'progress', component: ProgressComponent},
-    //         {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    //         {path: 'graficas1', component: Graficas1Component}
-
-    //     ]
-    // },
 
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
